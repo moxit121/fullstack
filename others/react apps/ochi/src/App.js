3544b22860f90f97b1c86f36926614b4ef2e7447
Moxit Shah <moxitshah121@gmail.com>
2024-02-13 08:49:02 +0530

    f

--- others/react apps/ochi/src/App.js
+++ others/react apps/ochi/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import LandingPage from './components/LandingPage'
+
 
 function App() {
   return (
-    <div>
-      ReactJS | Modern Animated Website | OCHI DESIGN | Awwwarded Website Clone
+    <div className='w-full h-screen bg-zinc-900 text-white'>
+      {/* ReactJS | Modern Animated Website | OCHI DESIGN | Awwwarded Website Clone */}
+    <Navbar /> 
+    <LandingPage />
     </div>
   )
 }
---
+++ others/react apps/ochi/src/components/LandingPage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div className='w-full h-screen  bg-zinc-700 border-t-2'>
+      <div classname='mt-40 px-20' >
+        <div className='masker '>
+          <h1>WE CREATE</h1>
+        </div>
+        <div className='masker'>
+          <h1>EYE OPENING</h1>
+        </div>
+        <div className='masker'>
+          <h1>PRESENTATIONS</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
